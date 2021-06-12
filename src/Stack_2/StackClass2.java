3ff3449809026ac4fd6ab4334f4b0981651a7d3b
Manupendra Tiwari <manupendratiwari@Manupendras-MacBook-Air.local>
2021-06-12 23:23:22 +0530

    added stack class 2 solution

--- src/Stack_2/StackClass2.java
+++ src/Stack_2/StackClass2.java
@@ -3,27 +3,100 @@ package Stack_2;
 import java.util.Stack;
 
 public class StackClass2 {
-    static void nextGreater(int[] a){
+    //Time Complexity: O(n)
+    //Space complexity: O(n)
+    static void nextGreater(int[] a) {
         Stack<Integer> nextGreaterEle = new Stack<>();
-        for (int i = 0; i < a.length; i++) {
-            if(nextGreaterEle.empty()){
-                nextGreaterEle.push(a[i]);
-            }else if(nextGreaterEle.peek()>a[i]){
-                while(nextGreaterEle.peek()>a[i]) {
-                    nextGreaterEle.push(a[i]);
+        nextGreaterEle.push(a[0]);
+        for (int i = 1; i < a.length; i++) {
+            int val = a[i];
+            if (!nextGreaterEle.isEmpty()) {
+                int ele = nextGreaterEle.pop();
+                while (ele < val) {
+                    System.out.println(ele + " ---->Next Greater Element is " + val);
+                    if (nextGreaterEle.isEmpty()) {
+                        break;
+                    }
+                    ele = nextGreaterEle.pop();
                 }
-            }else if(nextGreaterEle.peek()<a[i]){
-                while(nextGreaterEle.peek()<a[i]){
-                    System.out.println(nextGreaterEle.pop()+" whose next greater is: "+a[i]);
+                if (ele > val) {
+                    nextGreaterEle.push(ele);
                 }
             }
+            nextGreaterEle.push(val);
         }
-        while(!nextGreaterEle.isEmpty()){
-            System.out.println(nextGreaterEle.pop()+" whose next greater is: -1");
+
+        //For Leftover elements in stack
+        while (!nextGreaterEle.isEmpty()) {
+            int ele = nextGreaterEle.pop();
+            int nextGreater = -1;
+            System.out.println(ele + " ----> Next Greater Element is " + nextGreater);
         }
     }
 
+    static void evaluatePostFixExpression(String s) {
+        Stack<Integer> exp_ans = new Stack<>();
+        char ch = ' ';
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+                exp_ans.push((s.charAt(i))-'0');
+            } else {
+                int val1 = exp_ans.pop();
+                int val2 = exp_ans.pop();
+                ch=s.charAt(i);
+                switch(ch)
+                {
+                    case '+':
+                        exp_ans.push(val2+val1);
+                        break;
+
+                    case '-':
+                        exp_ans.push(val2- val1);
+                        break;
+
+                    case '/':
+                        exp_ans.push(val2/val1);
+                        break;
+
+                    case '*':
+                        exp_ans.push(val2*val1);
+                        break;
+                }
+
+            }
+        }
+        System.out.println(exp_ans.pop());
+    }
+
+    static int[] stockSpan(int []a){
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+        int output[]=new int[a.length];
+        output[0]=1;
+        for (int i = 1; i < a.length; i++) {
+            while(!stack.isEmpty()&& a[i]>a[stack.peek()]){
+                stack.pop();
+            }
+            if(stack.isEmpty()){
+                output[i]=i+1;
+            }else {
+                output[i]=i-stack.peek();
+            }
+            stack.push(i);
+        }
+        return output;
+    }
+
     public static void main(String[] args) {
-        nextGreater(new int[]{7,5,4,3,6});
+        //In this next greater function, Here the ordering is not maintain
+        //nextGreater(new int[]{7, 5, 4, 3, 6});
+
+        //evaluatePostFixExpression("123+*8-");
+
+        //Stock Span Problem
+        int[] res=stockSpan(new int[]{100,80,60,70,60,75,85});
+        for(int val:res){
+            System.out.print(val+" ");
+        }
     }
 }
